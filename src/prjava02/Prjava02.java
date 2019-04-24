a683f5fa42a4e57abe3aec7d2c476ea24e476e25
nesaes1718daw2vib <nesaes1718daw2vib2@gmail.com>
2019-04-24 16:38:08 +0200

    Treballant amb ordres Git i branques: Versió 1.2 del projecte prjava02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -31,5 +31,6 @@ public class Prjava02 {
 		System.out.println("Exception occurred");
         } 
         System.out.println("Creació d'una branca del projecte prjava02"); 
+        System.out.println("Afegint més codi a la branca00 del projecte prjava02");
     }
 }
